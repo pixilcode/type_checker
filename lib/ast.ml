6f90aa68c901e2d73a33f60f6e064ba993ba072a
Brendon Bown <brendon.bown@gmail.com>
2024-03-20 16:43:38 -0600

    Create AST

---
+++ lib/ast.ml
@@ -0,0 +1,33 @@
+type math_op =
+  | Add
+  | Subtract
+  | Multiply
+  | Divide
+
+type compare_op =
+  | Equal
+  | LessThan
+
+type logic_op =
+  | And
+  | Or
+
+type ident = string
+type type_ = string
+
+type expr =
+  | Number of int
+  | Boolean of bool
+  | Ident of ident
+  | BinaryMath of expr * math_op * expr
+  | BinaryCompare of expr * compare_op * expr
+  | BinaryLogic of expr * logic_op * expr
+  | Not of expr
+  | If of expr * expr * expr
+  | Let of (ident * expr) list * expr
+  | FnApp of expr * expr
+  | FnExpr of ident * type_ * expr
+  | Sequence of expr * expr
+  | Mutate of ident * expr
+  | ObjectExpr of (ident * expr) list
+  | FieldAccess of expr * ident
