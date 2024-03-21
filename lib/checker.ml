9f7646bf01fa2bd44d4f81ec473511f4f17cc97f
Brendon Bown <brendon.bown@gmail.com>
2024-03-21 01:15:17 -0600

    Add arithmetic type checking

--- lib/checker.ml
+++ lib/checker.ml
@@ -1,7 +1,32 @@
 open Core
 
-let check ast: (Type.t, string) result =
+let rec check ast: (Type.t, string) result =
+  let error message: ('a, string) result =
+    let message = (
+      "Squishy bananas! " ^
+      message
+    ) in
+    Error message
+  in
+
+  let open Result.Monad_infix in
   match ast with
   | Ast.Number _ -> Ok Type.Num
   | Ast.Boolean _ -> Ok Type.Bool
+  | Ast.BinaryMath (lhs, _, rhs) -> begin
+    check lhs >>= fun (lhs_type) ->
+    check rhs >>= fun (rhs_type) ->
+    match (lhs_type, rhs_type) with
+    | (Type.Num, Type.Num) -> Ok (Type.Num)
+    | _ ->
+      let lhs_type_string = Printer.type_to_string lhs_type in
+      let rhs_type_string = Printer.type_to_string rhs_type in
+      let message =
+        Printf.sprintf
+          "Invalid types %s and %s for arithmetic operator"
+          lhs_type_string
+          rhs_type_string
+      in
+      error message
+  end
   | _ -> failwith "unimplemented"
--- lib/parser.ml
+++ lib/parser.ml
@@ -10,21 +10,38 @@ let from_s_expr input: (Ast.expr, string) result =
     Error message
   in
 
+  let parse_error expr: ('a, string) result =
+    let expr = Sexp.to_string expr in
+    error ("Unable to parse `" ^ expr ^ "`")
+  in
+
   let is_number str =
     String.for_all ~f:Char.is_digit str
   in
   
-  let parse_expr input: (Ast.expr, string) result =
+  let rec parse_expr input: (Ast.expr, string) result =
     let open Sexp in
+    let open Result.Monad_infix in
     match input with
     | Atom "true" -> Ok (Ast.Boolean true)
     | Atom "false" -> Ok (Ast.Boolean false)
     | Atom value when is_number value ->
       let num = int_of_string value in
       Ok (Ast.Number num)
-    | expr ->
-      let expr = Sexp.to_string expr in
-      error ("Unable to parse `" ^ expr ^ "`")
+    | List [
+      Atom "+";
+      lhs;
+      rhs;
+    ] ->
+      parse_expr lhs >>= fun (parsed_lhs) ->
+      parse_expr rhs >>= fun (parsed_rhs) ->
+      let ast = Ast.BinaryMath (
+        parsed_lhs,
+        Ast.Add,
+        parsed_rhs
+      ) in
+      Ok ast
+    | expr -> parse_error expr
   in
 
   let input = Sexp.of_string input in
