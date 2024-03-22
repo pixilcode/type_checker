3e32aaad56f09296ddcb70ea27d937ebffd99214
Brendon Bown <brendon.bown@gmail.com>
2024-03-21 18:51:10 -0600

    Build main binary

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,29 @@
-let () = print_endline "Hello, World!"
+open Type_checker
+open Core
+
+let () =
+  let open Result.Monad_infix in
+
+  (* read stdin as a single string *)
+  In_channel.input_all In_channel.stdin
+
+  (* parse the input *)
+  |> Parser.from_s_expr
+
+  (* type check the input *)
+  >>= Checker.check ~env:(Env.empty ())
+
+  (* get the result type *)
+  >>| fst
+
+  (* convert the type to a string *)
+  >>| Printer.type_to_string
+
+  (* convert any errors to a string *)
+  |> Result.map_error ~f:Printer.error_to_string
+
+  (* print the result *)
+  |> function
+  | Ok output
+  | Error output -> print_endline output
+
